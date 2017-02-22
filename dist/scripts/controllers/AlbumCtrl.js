f0a2ab1ecb12fab402c2d15082b746ebc97aba91
John P <john.pickett@gmail.com>
2017-02-22 06:39:57 -0600

    angular ckp6 complete

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,9 @@
 (function (){
-    function AlbumCtrl() {
-        this.albumData = []
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum;
         this.albumData.push(albumPicasso);
     }
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
