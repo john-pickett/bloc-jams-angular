8bedbc83cd454d0e164d3fe63f22a3a357726b84
John P <john.pickett@gmail.com>
2017-02-24 10:01:45 -0600

    angular asg9 complete

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -51,6 +51,10 @@
                     $document.unbind('mouseup.thumb');
                     });
                 };
+                
+                scope.thumbStyle = function() {
+                    return {left: percentString()};
+                };
             }
         };
     }
